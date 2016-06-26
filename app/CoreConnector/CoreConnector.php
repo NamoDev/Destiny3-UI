c6ade3679babd7ed180e9391bdcfc7350ada4b82
theminer3746 <bwsminecraft@hotmail.com>
2016-06-26 22:52:28 +0700

    [W.I.P] Add model to send data to core

---
+++ app/CoreConnector/CoreConnector.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class CoreConnector extends Model{
+    public function sendDataToCore(){
+
+    }
+
+    private function makeCurlRequest(){
+        
+    }
+}
