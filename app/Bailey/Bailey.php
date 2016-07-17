b9cd07b2480830f5fe424de82a2ea1a4818b661a
Namo <namo@namodev.com>
2016-07-17 08:48:01 +0700

    Added new config option: 'Core base API URL'

--- app/Bailey/Bailey.php
+++ app/Bailey/Bailey.php
@@ -14,24 +14,25 @@ namespace App\Bailey;
 class Bailey{
 
 
+
     /**
      * doAPIrequest
      * @param  string $endpoint API endpoint to call to, without forward and trailing slashes.
      * @param  string $method HTTP method to use.
      * @param  array $variables key-value array of variables to send along with the API request.
-     * @param  string $baseURI The base URI/URL of the API server.
+     * @param  string $baseURL The base URI/URL of the API server.
      * @return string raw cURL result on query success. bool FALSE if access is forbidden.
      *
-     * note: this is a non-public function!
+     * note: this is not a public function!
      */
-    function doAPIrequest(string $endpoint, string $method, array $variables = [], string $baseURI = 'https://core3.destiny.tuent.triamudom.ac.th'){
+    function doAPIrequest(string $endpoint, string $method, array $variables = [], string $baseURL = Config::get("uiconfig.core_base_api_url")){
 
         // Get API Key
         $apiKey = Config::get("uiconfig.core_api_key");
 
         // Init cURL and set stuff:
         $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL,"$baseURI/$endpoint");
+        curl_setopt($ch, CURLOPT_URL,"$baseURL/$endpoint");
         curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $method);
 
         // VERY VERY IMPORTANT: the API key header.
@@ -42,13 +43,11 @@ class Bailey{
         curl_setopt($ch, CURLOPT_POSTFIELDS,$variables); // POST data field(s)
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
-        // Run baby run!
+        // run the cURL query
         $result = curl_exec($ch);
-
-        // Check return
         $returnHttpCode = curl_getinfo($handle, CURLINFO_HTTP_CODE);
 
-        // Is forbiden or unauthorized?
+        // Is result forbiden or unauthorized?
         if($returnHttpCode == 401 || $returnHttpCode == 403){
             return false;
         }else{
