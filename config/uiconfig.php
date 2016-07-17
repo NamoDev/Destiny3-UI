93e119d8d7aae9beacb64c3c110887b905cc5ce4
Namo <namo@namodev.com>
2016-07-17 08:43:51 +0700

    Added UI configuration file

---
+++ config/uiconfig.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+|
+| DestinyUI 3.0
+| (C) 2016 TUDT
+|
+| UI-Specific configurations
+|
+*/
+
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | DestinyCore API key
+    |--------------------------------------------------------------------------
+    |
+    | Here you can set the API key that we'll use when we need to call in to
+    | DestinyCore. This is a ##secret-data!##
+    |
+    */
+
+    'core_api_key' => 'debug',
+
+];
