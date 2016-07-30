19ac4cdc5809eb0db24b6496055c789234f93565
Namo <namo@namodev.com>
2016-07-30 12:15:07 +0700

    Updated layouts. Now comes with bootstrap & jQuery!

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,10 +1,13 @@
 <!doctype html>
 <html>
     <head>
-        <title>@yield('title')</title>
         <meta charset="utf-8" />
+        <title>@yield('title')</title>
+        <link rel="stylesheet" href="/assets/css/destinyui3.css" />
     </head>
     <body>
 
+        <script src="/assets/js/jquery.js"></script>
+        <script src="/assets/js/bootstrap.js"></script>
     </body>
 </html>
