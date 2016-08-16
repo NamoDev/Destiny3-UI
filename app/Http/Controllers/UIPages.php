4f45950a9e6ef387118a3b1ef0cb88596a0c9198
Namo <namo@namodev.com>
2016-08-16 10:58:11 +0700

    Added base route template

--- app/Http/Controllers/UIPages.php
+++ app/Http/Controllers/UIPages.php
@@ -13,13 +13,16 @@ namespace App\Http\Controllers;
 
 class UIPages extends Controller{
 
-  // Applicant home page (a.k.a. the "Dashboard")
-  public function applicantHomePage(){
-    return response()->view('applicant_home');
-  }
-
-    public function homepage(){
+    // Public homepage
+    public function homePage(){
         return response()->view('welcome');
     }
+
+    // Applicant home page (a.k.a. the "Dashboard")
+    public function applicantHomePage(){
+        return response()->view('applicant_home');
+    }
+
+
 
 }
