7e586fab611d9edc501ced7def1592ae055e9f2a
Namo <namo@namodev.com>
2016-08-16 10:58:11 +0700

    Added base route template

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -13,15 +13,50 @@
 
 
 /*
-| Front-end routes
+| Public accessible routes
+| TODO: Modify IE middleware to also disallow old Chromes and Firefoxes as well - they can also cause issues.
 */
 Route::group(['middleware' => ['web']], function(){
-    Route::get('/', 'UIPages@homepage');
+
+    // Homepage
+    Route::get('/', 'UIPages@homePage');
+
+    // Login page
+    Route::get('login', 'UIPages@loginPage');
+
 });
 
 /*
-| API Routes (V1.0)
+| Front-end pages for the applicant. All of these requires login.
+| TODO: Add login verification middleware
+*/
+Route::group(['prefix' => 'application', 'middleware' => ['web']], function(){
+    // Handle steps here
+});
+
+/*
+| API Routes (v1)
 */
 Route::group(['prefix' => 'api/v1', 'middleware' => ['web']], function(){
+
+    /*
+    NOTE: These routes won't work yet - there's no backend handler for it!
+    This is just for planning purposes.
+    */
+
+    // Account creation
+    Route::post('account/create', 'Blah@Blah');
+
+    // Login
+    Route::post('account/login', 'Blah@Blah');
+
+    // Get applicant data. Simple!
+    Route::get('applicant/data', 'Blah@Blah');
+
+    // Partial data submission. Probably would get JSON of everything and process accordingly.
+    Route::post('applicant/data', 'Blah@Blah');
+
+    // Submit complete data & get PDF. Using GET here 'cause the client will directly access this URL.
+    Route::get('applicant/submit', 'Blah@Blah');
 
 });
