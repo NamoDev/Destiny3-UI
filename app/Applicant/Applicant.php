8884749c4c0a25591b929314d6ffde1a5b7dea4b
Namo <namo@namodev.com>
2016-08-17 21:33:59 +0700

    Added base code for the API controller

--- app/Applicant/Applicant.php
+++ app/Applicant/Applicant.php
@@ -12,7 +12,38 @@ namespace App\Applicant;
 */
 
 use Config;
+use DB;
 
 class Applicant{
+
+    /*
+    | create()
+    | A method for creating an applicant.
+    | Requires a lot of variables. Hopefully, they should be self-explanatory:
+    */
+    public function create(
+        string $citizenID,
+        string $title,
+        string $firstName,
+        string $lastName,
+        string $title_en,
+        string $firstName_en,
+        string $lastName_en,
+        int $gender,
+        string $email,
+        string $phone,
+        int $birthday,
+        int $birthmonth,
+        int $birthyear
+    ){
+        // See if an applicant with this CitizenID exists in the system.
+        // If so, throw an exception:
+        if(DB::collection("applicants")->where("citizenID", $citizenID)->count() != 0){
+            throw new \Exception("Duplicate application requests are not allowed");
+        }else{
+            // Continue.
+
+        }
+    }
 
 }
