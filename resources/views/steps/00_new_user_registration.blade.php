69741a753bf80e77c778c87107349d18f7aa1d23
Namo <namo@namodev.com>
2016-08-18 19:58:08 +0700

    Changed first registration page theme

--- resources/views/steps/00_new_user_registration.blade.php
+++ resources/views/steps/00_new_user_registration.blade.php
@@ -1,2 +1,15 @@
-@extends('layouts.single_form')
+@extends('layouts.no_navbar')
 @section('title', 'สมัครใหม่')
+
+
+@section('content')
+<div class="row" style="margin-top:80px;">
+    <div class="col-md-12">
+        <h2>สมัครใหม่</h2>
+        <br />
+        <div class="flat-well">
+          
+        </div>
+    </div>
+</div>
+@endsection
