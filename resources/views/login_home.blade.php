bbf959adcd61b0b40ebf5ef449cb2205a23029a2
Namo <namo@namodev.com>
2016-08-18 19:53:07 +0700

    Added frontpage links

--- resources/views/login_home.blade.php
+++ resources/views/login_home.blade.php
@@ -22,7 +22,7 @@
 
 <div class="row" style="margin-top:15px;">
     <div class="col-md-6">
-        <form class="login-form" method="POST" action="/actions/login">
+        <form class="login-form" method="POST" action="/login">
             <legend>เข้าสู่ระบบ</legend>
             <div class="form-group">
                 <input type="text" class="form-control login-field" value="" placeholder="รหัสประจำตัวประชาชน" id="login-id" />
@@ -35,22 +35,22 @@
             </div>
 
             <button class="btn btn-primary btn-lg btn-block" type="submit">เข้าสู่ระบบ</button>
-            <a class="login-link" href="#">ลืมรหัสผ่าน?</a>
+            <a class="login-link" href="/iforgot">ลืมรหัสผ่าน?</a>
         </form>
     </div>
     <div class="col-md-6">
         <div class="login-form">
             <legend>ยังไม่เคยลงทะเบียน?</legend>
-            <a class="btn btn-primary btn-lg btn-block" href="#">สมัครใหม่</a>
+            <a class="btn btn-primary btn-lg btn-block" href="/application/begin">สมัครใหม่</a>
         </div>
         <div class="login-form" style="margin-top:10px;">
             <legend>ปัญหาในการสมัคร?</legend>
             <div class="row">
                 <div class="col-xs-6">
-                    <a class="btn btn-primary btn-lg btn-block" href="#">คำถามที่พบบ่อย</a>
+                    <a class="btn btn-primary btn-lg btn-block" href="/faq">คำถามที่พบบ่อย</a>
                 </div>
                 <div class="col-xs-6">
-                    <a class="btn btn-primary btn-lg btn-block" href="#">ติดต่อเจ้าหน้าที่</a>
+                    <a class="btn btn-primary btn-lg btn-block" href="/contact">ติดต่อเจ้าหน้าที่</a>
                 </div>
             </div>
         </div>
