512fb251380fb895133e81942c048812eed8b03f
Namo <namo@namodev.com>
2016-08-19 01:03:33 +0700

    [Test] forcing external URL

--- config/app.php
+++ config/app.php
@@ -39,7 +39,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'http://v3.destinyui.namodev.com'),
 
     /*
     |--------------------------------------------------------------------------
