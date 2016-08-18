d67efb206acafd166ac4a32ebbd15f4043597b76
Namo <namo@namodev.com>
2016-08-19 01:47:37 +0700

    [Debugging] add a route to delete test user in case of mishaps

--- app/Http/Controllers/DebugController.php
+++ app/Http/Controllers/DebugController.php
@@ -3,9 +3,17 @@ namespace App\Http\Controllers;
 
 use Applicant;
 use Illuminate\Http\Request;
+use DB;
+
 
 class DebugController extends Controller{
   public function show_phpinfo(){
     phpinfo();
   }
+
+  public function deleteTestingUser(){
+    DB::collection("applicants")->where("citizenid", "1111111111119")->delete();
+    echo("Done!");
+  }
+
 }
