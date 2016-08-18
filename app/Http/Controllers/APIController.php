314f016045246973f24c41ec4df7c6b19b4b4583
Namo <namo@namodev.com>
2016-08-19 01:43:31 +0700

    [Debugging] Now being a bit more precise on what's causing the login to fail

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -105,10 +105,10 @@ class APIController extends Controller{
         if($applicantInterface->login($request->login_name, $request->login_password)){
           return redirect('application/home');
         }else{
-          return redirect('/')->with('message', 'ชื่อผู้ใช้หรือรหัสผ่านไม่ถูกต้อง')->with('alert-class', 'alert-warning');
+          return redirect('/')->with('message', 'INVALID_USERNAME_OR_PASSWORD')->with('alert-class', 'alert-warning');
         }
       }catch(\Throwable $whatever){
-        return redirect('/')->with('message', 'ชื่อผู้ใช้หรือรหัสผ่านไม่ถูกต้อง')->with('alert-class', 'alert-warning');
+        return redirect('/')->with('message', 'LOGIN_EXCEPTION_THROWN')->with('alert-class', 'alert-warning');
       }
 
     }
