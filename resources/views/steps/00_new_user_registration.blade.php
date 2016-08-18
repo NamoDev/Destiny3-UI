dc70f47e9bcf46c73b8e1537266218ad43a5f5bc
Namo <namo@namodev.com>
2016-08-18 20:25:56 +0700

    Added procedural birthdate selection system

--- resources/views/steps/00_new_user_registration.blade.php
+++ resources/views/steps/00_new_user_registration.blade.php
@@ -8,8 +8,10 @@
         <h2>สมัครใหม่</h2>
         <br />
         <div class="flat-well">
+          <legend>ข้อมูลส่วนตัว</legend>
           <div class="row">
             <div class="col-md-2 col-xs-4">
+              <span class="help-block">คำนำหน้าชื่อ</span>
               <select id="title" name="title" class="form-control select select-primary select-block mbl">
                 <optgroup label="คำนำหน้าชื่อ">
                   <option value="0">ด.ช.</option>
@@ -21,9 +23,11 @@
               </select>
             </div>
             <div class="col-md-4 col-md-offset-1 col-xs-8">
+              <span class="help-block">ชื่อ</span>
               <input id="fname" name="fname" type="text" placeholder="ชื่อ" class="form-control" />
             </div>
             <div class="col-md-5 col-xs-12">
+              <span class="help-block">นามสกุล</span>
               <input id="lname" name="lname" type="text" placeholder="นามสกุล" class="form-control" />
             </div>
           </div>
@@ -33,12 +37,70 @@
               <!-- Hidden: english title text. Box here will appear only if the title is selected as "other" -->
             </div>
             <div class="col-md-4 col-md-offset-1 col-xs-8">
+              <span class="help-block">นามสกุล (ภาษาอังกฤษ)</span>
               <input id="fname_en" name="fname_en" type="text" placeholder="First name" class="form-control" />
             </div>
             <div class="col-md-5 col-xs-12">
+              <span class="help-block">นามสกุล (ภาษาอังกฤษ)</span>
               <input id="lname_en" name="lname_en" type="text" placeholder="Last name" class="form-control" />
             </div>
           </div>
+          <!-- == -->
+          <div class="row">
+            <div class="col-md-12">
+              <span class="help-block">รหัสประจำตัวประชาชน</span>
+              <input id="citizenid" name="citizenid" type="text" placeholder="รหัสประจำตัวประชาชน 13 หลัก" class="form-control" />
+            </div>
+          </div>
+          <!-- == -->
+          <div class="row">
+            <div class="col-md-12">
+              <span class="help-block">วัน เดือน ปีเกิด</span>
+              <div class="row">
+                <div class="col-xs-4">
+                  <select id="birthdate" name="birthdate" class="form-control select select-primary select-block mbl">
+                    <?php
+                    $date = 1;
+                    while($date <= 31){
+                      echo('<option value="' + $date + '">' + $date + '</option>');
+                      $date++;
+                    }
+                     ?>
+                  </select>
+                </div>
+                <div class="col-xs-4">
+                  <select id="birthmonth" name="birthmonth" class="form-control select select-primary select-block mbl">
+                    <option value="1">มกราคม</option>
+                    <option value="2">กุมภาพันธ์</option>
+                    <option value="3">มีนาคม</option>
+                    <option value="4">เมษายน</option>
+                    <option value="5">พฤษภาคม</option>
+                    <option value="6">มิถุนายน</option>
+                    <option value="7">กรกฎาคม</option>
+                    <option value="8">สิงหาคม</option>
+                    <option value="9">กันยายน</option>
+                    <option value="10">ตุลาคม</option>
+                    <option value="11">พฤศจิกายน</option>
+                    <option value="12">ธันวาคม</option>
+                  </select>
+                </div>
+                <div class="col-xs-4">
+                  <select id="birthyear" name="birthyear" class="form-control select select-primary select-block mbl">
+                    <?php
+                    $year = date("Y");
+                    $threshold = 30;
+                    while($threshold >= 0){
+                      echo('<option value="' + $year + '">' + $year + '</option>');
+                      $year -= 1;
+                      $threshold -= 1;
+                    }
+                     ?>
+                  </select>
+                </div>
+              </div>
+            </div>
+          </div>
+
         </div>
     </div>
 </div>
