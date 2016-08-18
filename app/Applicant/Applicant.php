6cab4b016c223ad67ca7d9f063701fbf1b20a3e9
Namo <namo@namodev.com>
2016-08-18 23:33:21 +0700

    Finished account creation/login/logout backend functions

--- app/Applicant/Applicant.php
+++ app/Applicant/Applicant.php
@@ -13,11 +13,13 @@ namespace App\Applicant;
 
 use Config;
 use DB;
+use Hash;
+use Session;
 
 class Applicant{
 
     /*
-    | create()
+    | create
     | A method for creating an applicant.
     | Requires a lot of variables. Hopefully, they should be self-explanatory:
     */
@@ -34,16 +36,87 @@ class Applicant{
         string $phone,
         int $birthday,
         int $birthmonth,
-        int $birthyear
+        int $birthyear,
+        string $password
     ){
         // See if an applicant with this CitizenID exists in the system.
         // If so, throw an exception:
-        if(DB::collection("applicants")->where("citizenID", $citizenID)->count() != 0){
+        if(DB::collection("applicants")->where("citizenid", $citizenID)->count() != 0){
             throw new \Exception("Duplicate application requests are not allowed");
         }else{
             // Continue.
-
+            // TODO: Implement any more checks as required
+            $insertID = DB::collection("applicants")->insertGetId([
+              'citizenid' => $citizenID,
+              'title' => $title,
+              'fname' => $firstName,
+              'lname' => $lastName,
+              'title_en' => $title_en,
+              'fname_en' => $firstName_en,
+              'lname_en' => $lastName_en,
+              'gender' => $gender,
+              'email' => $email,
+              'phone' => $phone,
+              'birthday' => $birthday,
+              'birthmonth' => $birthmonth,
+              'birthyear' => $birthyear,
+              'password' => Hash::make($password)
+            ]);
+            return string($insertID);
         }
     }
+
+    /*
+    | login
+    | Applicant login processor
+    | Requires citizenid and password. It's that simple!
+    */
+    public function login(string $citizenid, string $password){
+      // See if the user exists:
+      if(DB::collection("applicants")->where("citizenid", $citizenid)->count() == 1){
+        // OK. Password correct?
+        $loginUserData = DB::collection("applicants")->where("citizenid", $citizenid)->first();
+        if(Hash::check($password, $loginUserData->password)){
+          // Login OK
+          Session::put("applicant_logged_in", "1");
+          Session::put("applicant_citizen_id", $loginUserData->citizenid);
+          Session::put("applicant_full_name", $loginUserData->fname . " " . $loginUserData->lname);
+          return true;
+        }else{
+          // Login failed
+          return false;
+        }
+      }else{
+        // User does not exist
+        return false;
+      }
+    }
+
+    /*
+    | logout
+    | The complete opposite of login.
+    | No variables required.
+    */
+    public function logout(){
+      Session::flush();
+      Session::regenerate();
+      return true;
+    }
+
+    /*
+    | isLoggedIn
+    | Is the user logged in or not?
+    | No variables required.
+    */
+    public function isLoggedIn(){
+      if(Session::get("applicant_logged_in") == 1){
+        // Yep.
+        return true;
+      }else{
+        // Nope.
+        return false;
+      }
+    }
+
 
 }
--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -11,18 +11,43 @@
 
 namespace App\Http\Controllers;
 
+use Applicant;
+
 class APIController extends Controller{
-<<<<<<< Updated upstream
-  
-=======
+
 
     /*
     | Account creation API endpoint
     */
     public function createAccount(Request $request){
-        
+      // Validate incoming data:
+      $this->validate($request, [
+        'customtitle' => 'required',
+        'title' => 'required',
+        'fname' => 'required',
+        'lname' => 'required',
+        'title_en' => 'required',
+        'fname_en' => 'required',
+        'lname_en' => 'required',
+        'citizenid' => 'required',
+        'birthdate' => 'required',
+        'birthmonth' => 'required',
+        'birthyear' => 'required',
+        'email' => 'required',
+        'phone' => 'required',
+        'password' => 'required',
+        'password_confirm' => 'required',
+      ]);
+
+      // Double check password
+      if($request->input("password") != $request->input("password_confirm")){
+        abort(500, "Password doesn't match");
+      }
+
+      // TODO: Send back data on which field triggers an error
+
+
+
     }
 
-
->>>>>>> Stashed changes
 }
