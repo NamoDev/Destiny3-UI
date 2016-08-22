505e5a18541a58abbf4a11c912a18e782238b40f
Namo <namo@namodev.com>
2016-08-22 21:31:39 +0700

    Updated "change password" page base template

--- resources/views/change_password.blade.php
+++ resources/views/change_password.blade.php
@@ -1,22 +1,9 @@
-@extends('layouts.no_navbar')
+@extends('layouts.master')
 @section('title', 'หน้าหลัก')
 
 @section('content')
 
-<div class="row" style="margin-top:30px;">
-    <div class="col-md-12">
-        <h2>เปลี่ยนรหัสผ่าน</h2>
-    </div>
-</div>
-
-<div class="row" style="margin-top:18px;">
-    <div class="col-md-12">
-        <div class="flat-well">
-            <h5 style="margin:0px;font-weight:normal;">TUEnt "Destiny" <b>3.0.1</b></h5>
-        </div>
-    </div>
-    <br />
-</div>
+<h2>เปลี่ยนรหัสผ่าน</h2>
 
 
 @endsection
