06472e8eb086f9b90a7e297bc1415fe839edf510
Namo <namo@namodev.com>
2016-08-22 21:30:25 +0700

    Added base for password change page

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -28,11 +28,11 @@ https://tucc.triamudom.ac.th
 
 <div class="row" style="margin-top:18px;">
     <div class="col-md-12">
-        <div class="login-form">
+        <div class="flat-well">
             <h5 style="margin:0px;font-weight:normal;">TUEnt "Destiny" <b>3.0.1</b></h5>
         </div>
         <br />
-        <div class="login-form">
+        <div class="flat-well">
             <!-- start team -->
             <h5>ครูที่ปรึกษาโครงการ</h5>
             <div class="row">
