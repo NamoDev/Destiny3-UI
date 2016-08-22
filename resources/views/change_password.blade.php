53909a64b2a6be42c883f52e9105f3159a3fc982
Namo <namo@namodev.com>
2016-08-22 21:33:48 +0700

    Added base form for password change page

--- resources/views/change_password.blade.php
+++ resources/views/change_password.blade.php
@@ -3,7 +3,22 @@
 
 @section('content')
 
-<h2>เปลี่ยนรหัสผ่าน</h2>
-
+<legend>เปลี่ยนรหัสผ่าน</legend>
+<div class="row">
+  <div class="col-md-12" id="oldPasswordGroup">
+    <span class="help-block">รหัสผ่านปัจจุบัน</span>
+    <input id="oldPassword" name="oldPassword" type="password" placeholder="ใส่รหัสผ่านปัจจุบันของนักเรียน" class="form-control" />
+  </div>
+</div>
+<div class="row">
+  <div class="col-md-6" id="passwordGroup">
+    <span class="help-block">รหัสผ่านใหม่</span>
+    <input id="password" name="password" type="password" placeholder="กำหนดรหัสผ่าน" class="form-control" />
+  </div>
+  <div class="col-md-6" id="password_confirmGroup">
+    <span class="help-block">ยืนยันรหัสผ่านใหม่</span>
+    <input id="password_confirm" name="password_confirm" type="password" placeholder="กำหนดรหัสผ่านอีกครั้ง" class="form-control" />
+  </div>
+</div>
 
 @endsection
