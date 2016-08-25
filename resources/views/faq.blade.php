b910308a760135763fc1457f8a0a409023edf3dc
Namo <namo@namodev.com>
2016-08-25 15:34:54 +0700

    Added base FAQ page

---
+++ resources/views/faq.blade.php
@@ -0,0 +1,41 @@
+@extends('layouts.no_navbar')
+@section('title', 'คำถามที่พบบ่อย')
+
+@section('content')
+
+<!--
+  ________  ______  ______
+ /_  __/ / / / __ \/_  __/
+  / / / / / / / / / / /
+ / / / /_/ / /_/ / / /
+/_/  \____/_____/ /_/
+
+TUEnt "Destiny", (C) {{ date("Y") }} TUDT.
+
+The world could always use more heroes. Are you with us?
+https://tucc.triamudom.ac.th
+
+-->
+
+<div class="row" style="margin-top:30px;">
+    <div class="col-md-12">
+        <h3>คำถามที่พบบ่อย</h3>
+    </div>
+</div>
+
+<div class="row" style="margin-top:18px;">
+    <div class="col-md-12">
+        <div class="flat-well">
+
+        </div>
+    </div>
+    <br />
+</div>
+
+<div class="row" style="margin-top:15px;">
+    <div class="col-md-12">
+        <a href="/" class="btn btn-lg btn-block btn-primary">กลับหน้าหลัก</a>
+    </div>
+</div>
+
+@endsection
