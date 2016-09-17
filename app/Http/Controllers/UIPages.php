3a1e849bc4376fd11fc695af9bc80d86f6b6c5e5
Namo <namo@namodev.com>
2016-09-17 19:26:58 +0700

    Added base page for almost every step

--- app/Http/Controllers/UIPages.php
+++ app/Http/Controllers/UIPages.php
@@ -58,6 +58,43 @@ class UIPages extends Controller{
         return response()->view('steps.01_basic_information', ['applicantData' => $applicantData]);
     }
 
+    // Step 02: Applicant's parent information
+    public function step2_parentInfo(){
+        $applicantData = DB::collection("applicants")->where("citizenid", Session::get("applicant_citizen_id"))->first();
+        return response()->view('steps.02_parent_information', ['applicantData' => $applicantData]);
+    }
+
+    // Step 03: Applicant's address
+    public function step3_address(){
+        $applicantData = DB::collection("applicants")->where("citizenid", Session::get("applicant_citizen_id"))->first();
+        return response()->view('steps.03_address', ['applicantData' => $applicantData]);
+    }
+
+    // Step 04: Applicant's educational history
+    public function step4_educationHistory(){
+        $applicantData = DB::collection("applicants")->where("citizenid", Session::get("applicant_citizen_id"))->first();
+        return response()->view('steps.04_education_history', ['applicantData' => $applicantData]);
+    }
+
+    // Step 05: Plan selection
+    public function step5_planSelection(){
+        $applicantData = DB::collection("applicants")->where("citizenid", Session::get("applicant_citizen_id"))->first();
+        return response()->view('steps.05_plan_selection', ['applicantData' => $applicantData]);
+    }
+
+    // Step 06: Application day selection
+    public function step6_applicationDaySelection(){
+        $applicantData = DB::collection("applicants")->where("citizenid", Session::get("applicant_citizen_id"))->first();
+        return response()->view('steps.06_application_day_selection', ['applicantData' => $applicantData]);
+    }
+
+    // Step 07: Document upload & verification
+    public function step7_uploadDocuments(){
+        $applicantData = DB::collection("applicants")->where("citizenid", Session::get("applicant_citizen_id"))->first();
+        return response()->view('steps.07_document_upload', ['applicantData' => $applicantData]);
+    }
+
+
 
 
 
--- resources/views/steps/05_plan_selection.blade.php
+++ resources/views/steps/05_plan_selection.blade.php
@@ -1,1 +1,11 @@
 @extends('layouts.master')
+
+@section('title', 'เลือกแผนการเรียน')
+
+@section('content')
+<legend><i class="fa fa-institution"></i> เลือกแผนการเรียน <i class="fa fa-spinner fa-spin text-muted pull-right" style="display:none;" id="loadingSpinner"></i></legend>
+@endsection
+
+@section('additional_scripts')
+
+@endsection
