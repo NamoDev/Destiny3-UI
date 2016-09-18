b6d91754e9ca00e039fcc2a7c1322ab7d7304474
theminer3746 <bwsminecraft@hotmail.com>
2016-09-18 20:13:51 +0700

    Add recaptcha

--- config/app.php
+++ config/app.php
@@ -148,6 +148,8 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
 
+        Anhskohbo\NoCaptcha\NoCaptchaServiceProvider::class,
+
         /*
          * Application Service Providers...
          */
