41f60f20c3ac651497afa14dd8947da7015f83c7
Namo <namo@namodev.com>
2016-09-18 23:48:19 +0700

    Added school input box
    (TODO: Maybe change this to searchable select?)

--- resources/views/steps/04_education_history.blade.php
+++ resources/views/steps/04_education_history.blade.php
@@ -3,7 +3,30 @@
 @section('title', 'ประวัติการศึกษา')
 
 @section('content')
+
 <legend><i class="fa fa-graduation-cap"></i> ประวัติการศึกษา <i class="fa fa-spinner fa-spin text-muted pull-right" style="display:none;" id="loadingSpinner"></i></legend>
+
+<div class="row">
+    <div class="col-md-9 col-xs-12">
+        <span class="help-block">จบการศึกษาระดับชั้นมัธยมศึกษาปีที่ 3 จากโรงเรียน</span>
+        <input id="school" name="school" type="text" placeholder="ชื่อโรงเรียน" class="form-control" />
+    </div>
+    <div class="col-md-3 col-xs-12">
+        <span class="help-block">ปีที่จบหรือคาดว่าจะจบการศึกษา</span>
+        <select id="graduation_year" name="graduation_year" class="form-control select select-primary select-block mbl">
+          <?php
+          $year = date("Y") + 543; // Assuming that "date" will be in Christian Era.
+          $threshold = 30;
+          while($threshold >= 0){
+            echo("<option value=\"$year\">$year</option>");
+            $year -= 1;
+            $threshold -= 1;
+          }
+           ?>
+        </select>
+    </div>
+</div>
+
 @endsection
 
 @section('additional_scripts')
