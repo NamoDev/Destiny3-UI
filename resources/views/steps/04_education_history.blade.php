35c1a9b4c8df0cecce0eb04c99975694e35d848c
Namo <namo@namodev.com>
2016-09-25 12:54:06 +0700

    Select boxes should now scale responsively

--- resources/views/steps/04_education_history.blade.php
+++ resources/views/steps/04_education_history.blade.php
@@ -13,7 +13,7 @@
     </div>
     <div class="col-md-3 col-xs-12">
         <span class="help-block">ปีที่จบหรือคาดว่าจะจบการศึกษา</span>
-        <select id="graduation_year" name="graduation_year" class="form-control select select-primary select-block mbl">
+        <select id="graduation_year" name="graduation_year" style="width:100%;" class="form-control select select-primary select-block mbl">
           <?php
           $year = date("Y") + 543; // Assuming that "date" will be in Christian Era.
           $threshold = 30;
@@ -30,6 +30,36 @@
         <input id="gpa" name="gpa" type="text" placeholder="GPA ในรูปแบบ 0.00" class="form-control" />
     </div>
 </div>
+
+{{-- School move in date section, for use with Province Quota Operation Mode only --}}
+@if(Config::get("uiconfig.mode") == "province_quota")
+<div class="row">
+    <div class="col-md-6">
+        <span class="help-block">วันที่เริ่มเข้าศึกษา</span>
+        <div class="row">
+            <div class="col-xs-4">
+                <select id="moveinDay" name="moveinDay" style="width:100%;" class="form-control select select-primary select-block Wmbl">
+
+                </select>
+            </div>
+            <div class="col-xs-4">
+                <select id="moveinMonth" name="moveinMonth" style="width:100%;" class="form-control select select-primary select-block mbl">
+
+                </select>
+            </div>
+            <div class="col-xs-4">
+                <select id="moveinYear" name="moveinYear" style="width:100%;" class="form-control select select-primary select-block mbl">
+
+                </select>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-6">
+        <span class="help-block">จังหวัด (โรงเรียน)</span>
+
+    </div>
+</div>
+@endif
 
 @endsection
 
