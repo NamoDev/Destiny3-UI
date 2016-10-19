1b50aa0c02ec4b60d674ae90f132ea3e6524e38a
keenthekeen <keen@keendev.net>
2016-10-19 23:27:16 +0700

    Added PHPDocs and beautified things in App/ Set Composer's minimum requirement of PHP to v7.0.0 Added IDE helper Added .idea (PHPStorm's internal folder) to .gitignore

--- app/Bailey/Bailey.php
+++ app/Bailey/Bailey.php
@@ -7,55 +7,60 @@ namespace App\Bailey;
 | DestinyUI 3.0
 | (C) 2016 TUDT
 |
-| "Bailey", a.k.a. DestinyCore API client.
-|
 */
 
 use Config;
 
-class Bailey{
-
-
-
-    /**
-     * doAPIrequest
-     * @param  string $endpoint API endpoint to call to, without forward and trailing slashes.
-     * @param  string $method HTTP method to use.
-     * @param  array $variables key-value array of variables to send along with the API request.
-     * @param  string $baseURL The base URI/URL of the API server.
-     * @return string raw cURL result on query success. bool FALSE if access is forbidden.
-     *
-     * note: this is not a public function!
-     */
-    protected function doAPIrequest(string $endpoint, string $method, array $variables = [], string $baseURL = Config::get("uiconfig.core_base_api_url")){
-
-        // Get API Key
-        $apiKey = Config::get("uiconfig.core_api_key");
-
-        // Init cURL and set stuff:
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL,"$baseURL/$endpoint");
-        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $method);
-
-        // VERY VERY IMPORTANT: the API key header.
-        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-            "X-Auth-APIKey: $apiKey"
-        ));
-
-        curl_setopt($ch, CURLOPT_POSTFIELDS,$variables); // POST data field(s)
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-
-        // run the cURL query
-        $result = curl_exec($ch);
-        $returnHttpCode = curl_getinfo($handle, CURLINFO_HTTP_CODE);
-
-        // Is result ok or created?
-        if($returnHttpCode == 200 || $returnHttpCode == 201){
-            return $result;
-        }else{
-            return false;
-        }
-
-    }
-
+/**
+ * "Bailey", a.k.a. DestinyCore API client.
+ * @package App\Bailey
+ */
+class Bailey {
+	
+	
+	/**
+	 * doAPIrequest
+	 * @param  string $endpoint API endpoint to call to, without forward and trailing slashes.
+	 * @param  string $method HTTP method to use.
+	 * @param  array  $variables key-value array of variables to send along with the API request.
+	 * @param  string $baseURL The base URI/URL of the API server.
+	 * @return string raw cURL result on query success. bool FALSE if access is forbidden.
+	 *
+	 * note: this is not a public function!
+	 */
+	protected function doAPIrequest(string $endpoint, string $method, array $variables = [], string $baseURL = NULL) {
+		
+		if (is_null($baseURL)) {
+			$baseURL = Config::get("uiconfig.core_base_api_url");
+		}
+		
+		// Get API Key
+		$apiKey = Config::get("uiconfig.core_api_key");
+		
+		// Init cURL and set stuff:
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL, "$baseURL/$endpoint");
+		curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $method);
+		
+		// VERY VERY IMPORTANT: the API key header.
+		curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+			"X-Auth-APIKey: $apiKey"
+		));
+		
+		curl_setopt($ch, CURLOPT_POSTFIELDS, $variables); // POST data field(s)
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		
+		// run the cURL query
+		$result = curl_exec($ch);
+		$returnHttpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+		
+		// Is result ok or created?
+		if ($returnHttpCode == 200 || $returnHttpCode == 201) {
+			return $result;
+		} else {
+			return false;
+		}
+		
+	}
+	
 }
--- app/Providers/ValidationServiceProvider.php
+++ app/Providers/ValidationServiceProvider.php
@@ -8,141 +8,147 @@ use Exception;
 use Validator;
 use Illuminate\Support\ServiceProvider;
 
-class ValidationServiceProvider extends ServiceProvider
-{
-    /**
-     * Bootstrap the application services.
-     *
-     * @return void
-     */
-    public function boot(){
-        Validator::extendImplicit('required_if_province_quota', function ($attribute, $value, $parameters, $validator){
-            if(Config::get('uiconfig.mode') == 'province_quota'){
-                if(is_null($value)){
-                    return false;
-                }elseif(is_string($value) && trim($value) === ''){
-                    return false;
-                }elseif((is_array($value) || $value instanceof Countable) && count($value) < 1){
-                    return false;
-                }
-
-                return true;
-            }else{
-                return true; // Not province quota, pass by default
-            }
-        });
-
-        Validator::extend('within_date', function($attribute, $value, $parameters, $validator){
-            if(!is_string($value)){
-                return false;
-            }
-
-            // Convert value in $parameters array to be easily accessible
-            $parameters = array_reduce($parameters, function ($result, $item) {
-                list($key, $value) = array_pad(explode('=', $item, 2), 2, null);
-
-                $result[$key] = $value;
-
-                return $result;
-            });
-
-            if(!isset($parameters['before']) && !isset($parameters['after'])){
-                throw new Exception('before or after parameter expected. None given');
-            }
-
-            $input = explode('/', $value);
-            $input_year = $input[2];
-            $input_month = $input[1];
-            $input_day = $input[0];
-
-            if(isset($parameters['before'])){
-                $before = explode('/', $parameters['before']);
-                $before_year = $before[2];
-                $before_month = $before[1];
-                $before_day = $before[0];
-                if($before_year < $input_year){
-                    return false;
-                }else if($before_year == $input_year){
-                    if($before_month < $input_month){
-                        return false;
-                    }else if($before_month == $input_month){
-                        if($before_day < $input_day){
-                            return false;
-                        }
-                    }
-                }
-            }
-
-            if(isset($parameters['after'])){
-                $after = explode('/', $parameters['after']);
-                $after_year = $after[2];
-                $after_month = $after[1];
-                $after_day = $after[0];
-                if($after_year > $input_year){
-                    return false;
-                }else if($after_year == $input_year){
-                    if($after_month > $input_month){
-                        return false;
-                    }else if($after_month == $input_month){
-                        if($after_day > $input_day){
-                            return false;
-                        }
-                    }
-                }
-            }
-
-            return true;
-        });
-
-        Validator::extend('citizen_id', function($attribute, $value, $parameters, $validator){
-            if(strlen($citizen_id) == 13){ // First check the length
-
-                $natid = str_split($citizen_id); // And split that into array
-
-                /* Perform calculation. Multiply digits by their value */
-                $c1  = $natid[0] * 13;
-                $c2  = $natid[1] * 12;
-                $c3  = $natid[2] * 11;
-                $c4  = $natid[3] * 10;
-                $c5  = $natid[4] * 9;
-                $c6  = $natid[5] * 8;
-                $c7  = $natid[6] * 7;
-                $c8  = $natid[7] * 6;
-                $c9  = $natid[8] * 5;
-                $c10 = $natid[9] * 4;
-                $c11 = $natid[10] * 3;
-                $c12 = $natid[11] * 2;
-
-                /* Get all the values, mod them by 11, and verify it to digit 13. */
-                $val1 = $c1 + $c2 + $c3 + $c4 + $c5 + $c6 + $c7 + $c8 + $c9 + $c10 + $c11 + $c12;
-                $val2 = $val1 % 11;
-                $val3 = 11 - $val2;
-                $val4 = substr($val3, -1);
-
-                $checkdigit = $natid[12];
-
-                if($val4 == $checkdigit){ // Are we good?
-                    // Yes!
-                    return true;
-                }else{
-                    // NO!
-                    return false;
-                }
-
-            }else{
-                // Invalid length!
-                return false;
-            }
-        });
-    }
-
-    /**
-     * Register the application services.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        //
-    }
+class ValidationServiceProvider extends ServiceProvider {
+	/**
+	 * Bootstrap the application services.
+	 *
+	 * @return void
+	 */
+	public function boot() {
+		Validator::extendImplicit('required_if_province_quota', function ($attribute, $value, $parameters, $validator) {
+			if (Config::get('uiconfig.mode') == 'province_quota') {
+				if (is_null($value)) {
+					return false;
+				} elseif (is_string($value) && trim($value) === '') {
+					return false;
+				} elseif ((is_array($value) || $value instanceof Countable) && count($value) < 1) {
+					return false;
+				}
+				
+				return true;
+			} else {
+				return true; // Not province quota, pass by default
+			}
+		});
+		
+		Validator::extend('within_date', function ($attribute, $value, $parameters, $validator) {
+			if (!is_string($value)) {
+				return false;
+			}
+			
+			// Convert value in $parameters array to be easily accessible
+			$parameters = array_reduce($parameters, function ($result, $item) {
+				list($key, $value) = array_pad(explode('=', $item, 2), 2, NULL);
+				
+				$result[$key] = $value;
+				
+				return $result;
+			});
+			
+			if (!isset($parameters['before']) && !isset($parameters['after'])) {
+				throw new Exception('before or after parameter expected. None given');
+			}
+			
+			$input = explode('/', $value);
+			$input_year = $input[2];
+			$input_month = $input[1];
+			$input_day = $input[0];
+			
+			if (isset($parameters['before'])) {
+				$before = explode('/', $parameters['before']);
+				$before_year = $before[2];
+				$before_month = $before[1];
+				$before_day = $before[0];
+				if ($before_year < $input_year) {
+					return false;
+				} else {
+					if ($before_year == $input_year) {
+						if ($before_month < $input_month) {
+							return false;
+						} else {
+							if ($before_month == $input_month) {
+								if ($before_day < $input_day) {
+									return false;
+								}
+							}
+						}
+					}
+				}
+			}
+			
+			if (isset($parameters['after'])) {
+				$after = explode('/', $parameters['after']);
+				$after_year = $after[2];
+				$after_month = $after[1];
+				$after_day = $after[0];
+				if ($after_year > $input_year) {
+					return false;
+				} else {
+					if ($after_year == $input_year) {
+						if ($after_month > $input_month) {
+							return false;
+						} else {
+							if ($after_month == $input_month) {
+								if ($after_day > $input_day) {
+									return false;
+								}
+							}
+						}
+					}
+				}
+			}
+			
+			return true;
+		});
+		
+		Validator::extend('citizen_id', function ($attribute, $value, $parameters, $validator) {
+			if (strlen($citizen_id) == 13) { // First check the length
+				
+				$natid = str_split($citizen_id); // And split that into array
+				
+				/* Perform calculation. Multiply digits by their value */
+				$c1 = $natid[0] * 13;
+				$c2 = $natid[1] * 12;
+				$c3 = $natid[2] * 11;
+				$c4 = $natid[3] * 10;
+				$c5 = $natid[4] * 9;
+				$c6 = $natid[5] * 8;
+				$c7 = $natid[6] * 7;
+				$c8 = $natid[7] * 6;
+				$c9 = $natid[8] * 5;
+				$c10 = $natid[9] * 4;
+				$c11 = $natid[10] * 3;
+				$c12 = $natid[11] * 2;
+				
+				/* Get all the values, mod them by 11, and verify it to digit 13. */
+				$val1 = $c1 + $c2 + $c3 + $c4 + $c5 + $c6 + $c7 + $c8 + $c9 + $c10 + $c11 + $c12;
+				$val2 = $val1 % 11;
+				$val3 = 11 - $val2;
+				$val4 = substr($val3, -1);
+				
+				$checkdigit = $natid[12];
+				
+				if ($val4 == $checkdigit) { // Are we good?
+					// Yes!
+					return true;
+				} else {
+					// NO!
+					return false;
+				}
+				
+			} else {
+				// Invalid length!
+				return false;
+			}
+		});
+	}
+	
+	/**
+	 * Register the application services.
+	 *
+	 * @return void
+	 */
+	public function register() {
+		//
+	}
 }
