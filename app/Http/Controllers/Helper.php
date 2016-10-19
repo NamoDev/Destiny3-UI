6f41606235522a1b319a76457272abd4f9deaa21
keenthekeen <keen@keendev.net>
2016-10-19 23:27:16 +0700

    Added PHPDocs and beautified things in App/ Set Composer's minimum requirement of PHP to v7.0.0 Added IDE helper Added .idea (PHPStorm's internal folder) to .gitignore

--- app/Http/Controllers/Helper.php
+++ app/Http/Controllers/Helper.php
@@ -15,218 +15,219 @@ use Applicant;
 use DB;
 use Session;
 
-class Helper extends Controller{
-
-    public static function checkStepCompletion(int $step){
-        try{
-            $applicantData = DB::collection("applicants")->where("citizenid", Session::get("applicant_citizen_id"))->first();
-            if(in_array($step, $applicantData['steps_completed'])){
-                return true;
-            }else{
-                return false;
-            }
-        }catch(\Throwable $whatever){
-            return false;
-        }
-    }
-
-    public static function formatCitizenIDforDisplay(string $citizenID){
-        try{
-            $splitted = str_split($citizenID);
-            return $splitted[0] . " - " . $splitted[1] . $splitted[2] . $splitted[3] . $splitted[4] . " - " . $splitted[5] . $splitted[6] . $splitted[7] . $splitted[8] . $splitted[9] . " - " . $splitted[10] . $splitted[11]. " - " . $splitted[12];
-        }catch(\Throwable $wtf){
-            return $citizenID;
-        }
-
-    }
-
-    public static function printQuotaSelectBox($id = NULL){
-        $A = array(
-            'ความสามารถพิเศษด้านวิชาการ',
-            'ความสามารถพิเศษด้านกีฬา',
-            'ความสามารถพิเศษด้านศิลปะ ดนตรี และนาฎศิลป์'
-        );
-        if (is_null($id)) {
-            for ($I = 0; $I <= 2; $I++) {
-                echo '<optgroup label="' . $A[$I] . '">';
-                switch ($I) {
-                    case 0:
-                        $num = array(
-                            'คณิตศาสตร์',
-                            'วิทยาศาสตร์',
-                            'คอมพิวเตอร์'
-                        );
-                        for ($i = 1; $i <= 3; $i++) {
-                            echo '<option value="' . $i . '">' . $num[$i - 1] . '</option>';
-                        }
-                        break;
-                    case 1:
-                        $num = array(
-                            'เทนนิส',
-                            'ว่ายน้ำ',
-                            'กอล์ฟ',
-                            'ยิงปืน',
-                            'เทเบิลเทนนิส',
-                            'เทควันโด',
-                            'ยิมนาสติกลีลา',
-                            'แบดมินตัน',
-                            'ฟุตบอล',
-                            'บาสเกตบอล',
-                            'วอลเล่ย์บอล(ชาย)'
-                        );
-                        for ($i = 4; $i <= 14; $i++) {
-                            echo '<option value="' . $i . '">' . $num[$i - 4] . '</option>';
-                        }
-                        break;
-                    case 2:
-                        $num = array(
-                            'นาฏศิลป์',
-                            'ศิลปะ',
-                            'ดนตรีไทย-ขลุ่ยเพียงออ',
-                            'ดนตรีไทย-ขับร้องเพลงไทยเดิม',
-                            'ดนตรีไทย-ขิม',
-                            'ดนตรีไทย-เครื่องหนัง',
-                            'ดนตรีไทย-ฆ้องวงใหญ่',
-                            'ดนตรีไทย-จะเข้',
-                            'ดนตรีไทย-ซอด้วง',
-                            'ดนตรีไทย-ซอสามสาย',
-                            'ดนตรีไทย-ซออู้',
-                            'ดนตรีไทย-ระนาดทุ้ม',
-                            'ดนตรีไทย-ระนาดเอก',
-                            'ดนตรีสากล-กีตาร์ไฟฟ้า',
-                            'ดนตรีสากล-กีตาร์เบสไฟฟ้า',
-                            'ดนตรีสากล-คีย์บอร์ด',
-                            'ดนตรีสากล-กลองชุด',
-                            'ดนตรีสากล-ขับร้องเพลงไทยสากล',
-                            'ดุริยางค์-Alto Saxophone',
-                            'ดุริยางค์-Clarinet',
-                            'ดุริยางค์-Flute',
-                            'ดุริยางค์-French Horn',
-                            'ดุริยางค์-Oboe',
-                            'ดุริยางค์-Percussion',
-                            'ดุริยางค์-Piccolo',
-                            'ดุริยางค์-Tenor Saxophone',
-                            'ดุริยางค์-Trombone',
-                            'ดุริยางค์-Trumpet',
-                            'ดุริยางค์-Tuba',
-                            'ดุริยางค์-อื่นๆ'
-                        );
-                        for ($i = 15; $i <= 44; $i++) {
-                            echo '<option value="' . $i . '">' . $num[$i - 15] . '</option>';
-                        }
-                        break;
-                }
-                echo '</optgroup>';
-            }
-        } else {
-            /*
-            if($i!=99){
-            echo '<option value="99">นักเรียนโควตาจังหวัด สพม.</option>';
-            }else{
-            echo '<option value="99" selected> นักเรียนโควตาจังหวัด สพม.</option>';
-            }
-            */
-            for ($I = 0; $I <= 2; $I++) {
-                echo '<optgroup label="' . $A[$I] . '">';
-                switch ($I) {
-                    case 0:
-                        for ($i = 1; $i <= 3; $i++) {
-                            $num = array(
-                                'คณิตศาสตร์',
-                                'วิทยาศาสตร์',
-                                'คอมพิวเตอร์'
-                            );
-                            if ($i != $id) {
-                                echo '<option value="' . $i . '">' . $num[$i - 1] . '</option>';
-                            } else {
-                                echo '<option value="' . $i . '" selected>' . $num[$i - 1] . '</option>';
-                            }
-                        }
-                        break;
-                    case 1:
-                        for ($i = 4; $i <= 14; $i++) {
-                            $num = array(
-                                'เทนนิส',
-                                'ว่ายน้ำ',
-                                'กอล์ฟ',
-                                'ยิงปืน',
-                                'เทเบิลเทนนิส',
-                                'เทควันโด',
-                                'ยิมนาสติกลีลา',
-                                'แบดมินตัน',
-                                'ฟุตบอล',
-                                'บาสเกตบอล',
-                                'วอลเล่ย์บอล(ชาย)'
-                            );
-                            if ($i != $id) {
-                                echo '<option value="' . $i . '">' . $num[$i - 4] . '</option>';
-                            } else {
-                                echo '<option value="' . $i . '" selected>' . $num[$i - 4] . '</option>';
-                            }
-                        }
-                        break;
-                    case 2:
-                        for ($i = 15; $i <= 44; $i++) {
-                            $num = array(
-                                'นาฏศิลป์',
-                                'ศิลปะ',
-                                'ดนตรีไทย-ขลุ่ยเพียงออ',
-                                'ดนตรีไทย-ขับร้องเพลงไทยเดิม',
-                                'ดนตรีไทย-ขิม',
-                                'ดนตรีไทย-เครื่องหนัง',
-                                'ดนตรีไทย-ฆ้องวงใหญ่',
-                                'ดนตรีไทย-จะเข้',
-                                'ดนตรีไทย-ซอด้วง',
-                                'ดนตรีไทย-ซอสามสาย',
-                                'ดนตรีไทย-ซออู้',
-                                'ดนตรีไทย-ระนาดทุ้ม',
-                                'ดนตรีไทย-ระนาดเอก',
-                                'ดนตรีสากล-กีตาร์ไฟฟ้า',
-                                'ดนตรีสากล-กีตาร์เบสไฟฟ้า',
-                                'ดนตรีสากล-คีย์บอร์ด',
-                                'ดนตรีสากล-กลองชุด',
-                                'ดนตรีสากล-ขับร้องเพลงไทยสากล',
-                                'ดุริยางค์-Alto Saxophone',
-                                'ดุริยางค์-Clarinet',
-                                'ดุริยางค์-Flute',
-                                'ดุริยางค์-French Horn',
-                                'ดุริยางค์-Oboe',
-                                'ดุริยางค์-Percussion',
-                                'ดุริยางค์-Piccolo',
-                                'ดุริยางค์-Tenor Saxophone',
-                                'ดุริยางค์-Trombone',
-                                'ดุริยางค์-Trumpet',
-                                'ดุริยางค์-Tuba',
-                                'ดุริยางค์-อื่นๆ'
-                            );
-                            if ($i != $id) {
-                                echo '<option value="' . $i . '">' . $num[$i - 15] . '</option>';
-                            } else {
-                                echo '<option value="' . $i . '" selected>' . $num[$i - 15] . '</option>';
-                            }
-                        }
-                        break;
-                }
-                echo '</optgroup>';
-            }
-        }
-    }
-
-    public static function voiceLines(){
-        $index = rand(0,4);
-        $lines = [
-            "*Beep boop*",
-            "Houston, we have a problem.",
-            "Oh, let's break it down!",
-            "Just in time.",
-            "Aw, rubbish!"
-        ];
-
-        try{
-            return $lines[$index];
-        }catch(\Throwable $wait_what){
-            return $lines[0];
-        }
-    }
-
+class Helper extends Controller {
+	
+	public static function checkStepCompletion(int $step) {
+		try {
+			$applicantData = DB::collection("applicants")->where("citizenid", Session::get("applicant_citizen_id"))->first();
+			if (in_array($step, $applicantData['steps_completed'])) {
+				return true;
+			} else {
+				return false;
+			}
+		} catch (\Throwable $whatever) {
+			return false;
+		}
+	}
+	
+	public static function formatCitizenIDforDisplay(string $citizenID) {
+		try {
+			$splitted = str_split($citizenID);
+			
+			return $splitted[0] . " - " . $splitted[1] . $splitted[2] . $splitted[3] . $splitted[4] . " - " . $splitted[5] . $splitted[6] . $splitted[7] . $splitted[8] . $splitted[9] . " - " . $splitted[10] . $splitted[11] . " - " . $splitted[12];
+		} catch (\Throwable $wtf) {
+			return $citizenID;
+		}
+		
+	}
+	
+	public static function printQuotaSelectBox($id = NULL) {
+		$A = array(
+			'ความสามารถพิเศษด้านวิชาการ',
+			'ความสามารถพิเศษด้านกีฬา',
+			'ความสามารถพิเศษด้านศิลปะ ดนตรี และนาฎศิลป์'
+		);
+		if (is_null($id)) {
+			for ($I = 0; $I <= 2; $I++) {
+				echo '<optgroup label="' . $A[$I] . '">';
+				switch ($I) {
+					case 0:
+						$num = array(
+							'คณิตศาสตร์',
+							'วิทยาศาสตร์',
+							'คอมพิวเตอร์'
+						);
+						for ($i = 1; $i <= 3; $i++) {
+							echo '<option value="' . $i . '">' . $num[$i - 1] . '</option>';
+						}
+						break;
+					case 1:
+						$num = array(
+							'เทนนิส',
+							'ว่ายน้ำ',
+							'กอล์ฟ',
+							'ยิงปืน',
+							'เทเบิลเทนนิส',
+							'เทควันโด',
+							'ยิมนาสติกลีลา',
+							'แบดมินตัน',
+							'ฟุตบอล',
+							'บาสเกตบอล',
+							'วอลเล่ย์บอล(ชาย)'
+						);
+						for ($i = 4; $i <= 14; $i++) {
+							echo '<option value="' . $i . '">' . $num[$i - 4] . '</option>';
+						}
+						break;
+					case 2:
+						$num = array(
+							'นาฏศิลป์',
+							'ศิลปะ',
+							'ดนตรีไทย-ขลุ่ยเพียงออ',
+							'ดนตรีไทย-ขับร้องเพลงไทยเดิม',
+							'ดนตรีไทย-ขิม',
+							'ดนตรีไทย-เครื่องหนัง',
+							'ดนตรีไทย-ฆ้องวงใหญ่',
+							'ดนตรีไทย-จะเข้',
+							'ดนตรีไทย-ซอด้วง',
+							'ดนตรีไทย-ซอสามสาย',
+							'ดนตรีไทย-ซออู้',
+							'ดนตรีไทย-ระนาดทุ้ม',
+							'ดนตรีไทย-ระนาดเอก',
+							'ดนตรีสากล-กีตาร์ไฟฟ้า',
+							'ดนตรีสากล-กีตาร์เบสไฟฟ้า',
+							'ดนตรีสากล-คีย์บอร์ด',
+							'ดนตรีสากล-กลองชุด',
+							'ดนตรีสากล-ขับร้องเพลงไทยสากล',
+							'ดุริยางค์-Alto Saxophone',
+							'ดุริยางค์-Clarinet',
+							'ดุริยางค์-Flute',
+							'ดุริยางค์-French Horn',
+							'ดุริยางค์-Oboe',
+							'ดุริยางค์-Percussion',
+							'ดุริยางค์-Piccolo',
+							'ดุริยางค์-Tenor Saxophone',
+							'ดุริยางค์-Trombone',
+							'ดุริยางค์-Trumpet',
+							'ดุริยางค์-Tuba',
+							'ดุริยางค์-อื่นๆ'
+						);
+						for ($i = 15; $i <= 44; $i++) {
+							echo '<option value="' . $i . '">' . $num[$i - 15] . '</option>';
+						}
+						break;
+				}
+				echo '</optgroup>';
+			}
+		} else {
+			/*
+			if($i!=99){
+			echo '<option value="99">นักเรียนโควตาจังหวัด สพม.</option>';
+			}else{
+			echo '<option value="99" selected> นักเรียนโควตาจังหวัด สพม.</option>';
+			}
+			*/
+			for ($I = 0; $I <= 2; $I++) {
+				echo '<optgroup label="' . $A[$I] . '">';
+				switch ($I) {
+					case 0:
+						for ($i = 1; $i <= 3; $i++) {
+							$num = array(
+								'คณิตศาสตร์',
+								'วิทยาศาสตร์',
+								'คอมพิวเตอร์'
+							);
+							if ($i != $id) {
+								echo '<option value="' . $i . '">' . $num[$i - 1] . '</option>';
+							} else {
+								echo '<option value="' . $i . '" selected>' . $num[$i - 1] . '</option>';
+							}
+						}
+						break;
+					case 1:
+						for ($i = 4; $i <= 14; $i++) {
+							$num = array(
+								'เทนนิส',
+								'ว่ายน้ำ',
+								'กอล์ฟ',
+								'ยิงปืน',
+								'เทเบิลเทนนิส',
+								'เทควันโด',
+								'ยิมนาสติกลีลา',
+								'แบดมินตัน',
+								'ฟุตบอล',
+								'บาสเกตบอล',
+								'วอลเล่ย์บอล(ชาย)'
+							);
+							if ($i != $id) {
+								echo '<option value="' . $i . '">' . $num[$i - 4] . '</option>';
+							} else {
+								echo '<option value="' . $i . '" selected>' . $num[$i - 4] . '</option>';
+							}
+						}
+						break;
+					case 2:
+						for ($i = 15; $i <= 44; $i++) {
+							$num = array(
+								'นาฏศิลป์',
+								'ศิลปะ',
+								'ดนตรีไทย-ขลุ่ยเพียงออ',
+								'ดนตรีไทย-ขับร้องเพลงไทยเดิม',
+								'ดนตรีไทย-ขิม',
+								'ดนตรีไทย-เครื่องหนัง',
+								'ดนตรีไทย-ฆ้องวงใหญ่',
+								'ดนตรีไทย-จะเข้',
+								'ดนตรีไทย-ซอด้วง',
+								'ดนตรีไทย-ซอสามสาย',
+								'ดนตรีไทย-ซออู้',
+								'ดนตรีไทย-ระนาดทุ้ม',
+								'ดนตรีไทย-ระนาดเอก',
+								'ดนตรีสากล-กีตาร์ไฟฟ้า',
+								'ดนตรีสากล-กีตาร์เบสไฟฟ้า',
+								'ดนตรีสากล-คีย์บอร์ด',
+								'ดนตรีสากล-กลองชุด',
+								'ดนตรีสากล-ขับร้องเพลงไทยสากล',
+								'ดุริยางค์-Alto Saxophone',
+								'ดุริยางค์-Clarinet',
+								'ดุริยางค์-Flute',
+								'ดุริยางค์-French Horn',
+								'ดุริยางค์-Oboe',
+								'ดุริยางค์-Percussion',
+								'ดุริยางค์-Piccolo',
+								'ดุริยางค์-Tenor Saxophone',
+								'ดุริยางค์-Trombone',
+								'ดุริยางค์-Trumpet',
+								'ดุริยางค์-Tuba',
+								'ดุริยางค์-อื่นๆ'
+							);
+							if ($i != $id) {
+								echo '<option value="' . $i . '">' . $num[$i - 15] . '</option>';
+							} else {
+								echo '<option value="' . $i . '" selected>' . $num[$i - 15] . '</option>';
+							}
+						}
+						break;
+				}
+				echo '</optgroup>';
+			}
+		}
+	}
+	
+	public static function voiceLines() {
+		$index = rand(0, 4);
+		$lines = [
+			"*Beep boop*",
+			"Houston, we have a problem.",
+			"Oh, let's break it down!",
+			"Just in time.",
+			"Aw, rubbish!"
+		];
+		
+		try {
+			return $lines[$index];
+		} catch (\Throwable $wait_what) {
+			return $lines[0];
+		}
+	}
+	
 }
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -28,46 +28,46 @@ Route::any('application/sst/VFVEVF84MA==', 'UserController@ganymede'); // Easter
 /*
 Front-end pages for logged in applicants
 */
-Route::group(['prefix' => 'application', 'middleware' => ['web', 'auth']], function(){
-
-    Route::get('home', 'UIPages@applicantDashboard'); // Dashboard
-    Route::get('info', 'UIPages@step1_basicInfo'); // Step 1 : basic information
-    Route::get('parent', 'UIPages@step2_parentInfo'); // Step 2 : parent information
-    Route::get('address', 'UIPages@step3_address'); // Step 3 : address
-    Route::get('education', 'UIPages@step4_educationHistory'); // Step 4 : education history
-    Route::get('plan', 'UIPages@step5_planSelection'); // Step 5 : basic information
-    Route::get('day', 'UIPages@step6_applicationDaySelection'); // Step 6 : basic information
-    Route::get('documents', 'UIPages@step7_uploadDocuments'); // Step 7 : upload documents
-    Route::get('change_password', 'UIPages@changePasswordPage'); // Change password
-
+Route::group(['prefix' => 'application', 'middleware' => ['web', 'auth']], function () {
+	
+	Route::get('home', 'UIPages@applicantDashboard'); // Dashboard
+	Route::get('info', 'UIPages@step1_basicInfo'); // Step 1 : basic information
+	Route::get('parent', 'UIPages@step2_parentInfo'); // Step 2 : parent information
+	Route::get('address', 'UIPages@step3_address'); // Step 3 : address
+	Route::get('education', 'UIPages@step4_educationHistory'); // Step 4 : education history
+	Route::get('plan', 'UIPages@step5_planSelection'); // Step 5 : basic information
+	Route::get('day', 'UIPages@step6_applicationDaySelection'); // Step 6 : basic information
+	Route::get('documents', 'UIPages@step7_uploadDocuments'); // Step 7 : upload documents
+	Route::get('change_password', 'UIPages@changePasswordPage'); // Change password
+	
 });
 
 /*
 | API Routes (v1)
 */
-Route::group(['prefix' => 'api/v1', 'middleware' => ['web']], function(){
-
-    /*
-    NOTE: These routes won't work yet - there's no backend handler for it!
-    This is just for planning purposes.
-    */
-
-    // Account creation
-    Route::post('account/create', 'UserController@createAccount');
-
-    // TODO: Add API authentication middlewares for API routes below:
-
-    // Get applicant data. Simple!
-    Route::get('applicant/data', 'UserController@getApplicantData')->middleware('apiauth');
-
-    // Partial data submission. Probably would get JSON of everything and process accordingly.
-    Route::post('applicant/data', 'UserController@updateApplicantData')->middleware('apiauth');
-
-    // Submit complete data & get PDF. Using GET here 'cause the client will directly access this URL.
-    Route::get('applicant/submit', 'Blah@Blah')->middleware('apiauth');
-
-    // Password change handler
-    Route::post('account/change_password', 'UserController@changePassword')->middleware('apiauth');
-
-
+Route::group(['prefix' => 'api/v1', 'middleware' => ['web']], function () {
+	
+	/*
+	NOTE: These routes won't work yet - there's no backend handler for it!
+	This is just for planning purposes.
+	*/
+	
+	// Account creation
+	Route::post('account/create', 'UserController@createAccount');
+	
+	// TODO: Add API authentication middlewares for API routes below:
+	
+	// Get applicant data. Simple!
+	Route::get('applicant/data', 'UserController@getApplicantData')->middleware('apiauth');
+	
+	// Partial data submission. Probably would get JSON of everything and process accordingly.
+	Route::post('applicant/data', 'UserController@updateApplicantData')->middleware('apiauth');
+	
+	// Submit complete data & get PDF. Using GET here 'cause the client will directly access this URL.
+	Route::get('applicant/submit', 'Blah@Blah')->middleware('apiauth');
+	
+	// Password change handler
+	Route::post('account/change_password', 'UserController@changePassword')->middleware('apiauth');
+	
+	
 });
