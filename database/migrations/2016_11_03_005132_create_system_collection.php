7bfefe78f6d65d54f17d322479443cc3e35202e7
Namo <namo@namodev.com>
2016-11-03 00:53:35 +0700

    Added system collection migration code

---
+++ database/migrations/2016_11_03_005132_create_system_collection.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateApplicantsCollection extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('system', function ($collection) {
+            $collection->unique('config_item');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('system');
+    }
+}
