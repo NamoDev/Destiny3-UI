84cc3f81439692bd01a1327618900a822a7058e8
theminer3746 <bwsminecraft@hotmail.com>
2016-11-04 23:00:50 +0700

    Add file upload placeholder

--- resources/views/steps/07_document_upload.blade.php
+++ resources/views/steps/07_document_upload.blade.php
@@ -4,6 +4,10 @@
 
 @section('content')
 <legend><i class="fa fa-upload"></i> อัพโหลดเอกสารประกอบ <i class="fa fa-spinner fa-spin text-muted pull-right" style="display:none;" id="loadingSpinner"></i></legend>
+<div class="row">
+    <label class="control-label">Select File</label>
+    <input id="input-1a" type="file" class="file" data-show-preview="false">
+</div>
 @endsection
 
 @section('additional_scripts')
