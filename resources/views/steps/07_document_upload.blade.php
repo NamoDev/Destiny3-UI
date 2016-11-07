eb5fa7c398ac3e0904a11be1169ac064d0709a3c
Namo <namo@namodev.com>
2016-11-08 00:12:59 +0700

    Added Dropzone's CSS & some placebo's

--- resources/views/steps/07_document_upload.blade.php
+++ resources/views/steps/07_document_upload.blade.php
@@ -4,6 +4,16 @@
 
 @section('content')
 <legend><i class="fa fa-upload"></i> อัพโหลดเอกสารประกอบ <i class="fa fa-spinner fa-spin text-muted pull-right" style="display:none;" id="loadingSpinner"></i></legend>
+
+<div class="row">
+    <div class="col-md-6">
+        <form action="/file-upload" class="dropzone" id="file_picture"></form>
+    </div>
+    <div class="col-md-6">
+        <form action="/file-upload" class="dropzone" id="file_form1"></form>
+    </div>
+</div>
+
 <div class="row">
     <label class="control-label">Select File</label>
     <input id="input-1a" type="file" class="file" data-show-preview="false">
@@ -11,5 +21,12 @@
 @endsection
 
 @section('additional_scripts')
-
+<script src="/assets/js/dropzone.js"></script>
+<script>
+Dropzone.options.file_picture = {
+  maxFilesize: 2, // MB
+  maxFiles: 1,
+  addRemoveLinks: true
+};
+</script>
 @endsection
