0d5638e052e965f37930ca5d24e06e22d4fc57f2
theminer3746 <bwsminecraft@hotmail.com>
2016-11-08 13:54:35 +0700

    Fix database migration

--- database/migrations/2016_11_08_004103_create_application_days_collection.php
+++ database/migrations/2016_11_08_004103_create_application_days_collection.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateApplicantsCollection extends Migration
+class CreateApplicationDaysCollection extends Migration
 {
     /**
      * Run the migrations.
--- database/migrations/2016_11_08_004535_create_system_collection.php
+++ database/migrations/2016_11_08_004535_create_system_collection.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateApplicantsCollection extends Migration
+class CreateSystemCollection extends Migration
 {
     /**
      * Run the migrations.
