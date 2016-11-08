28bdb597e6a79bb5a7edb6a8968e589a744448cb
theminer3746 <bwsminecraft@hotmail.com>
2016-11-08 16:42:32 +0700

    Edit nav bar to change depend on which mode is being use

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -7,7 +7,7 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="/">ระบบรับสมัครนักเรียน โรงเรียนเตรียมอุดมศึกษา @if(Config::get('app.debug') === true) <sup><span class="badge"><i class="fa fa-crosshairs"></i> Debug Mode</span></sup> @endif</a>
+      <a class="navbar-brand" href="/">ระบบรับสมัครนักเรียน@if(Config::get('uiconfig.mode') == 'province_quota') โควตาจังหวัด @endifโรงเรียนเตรียมอุดมศึกษา @if(Config::get('app.debug') === true) <sup><span class="badge"><i class="fa fa-crosshairs"></i> Debug Mode</span></sup> @endif</a>
     </div>
 
     <!-- Collect the nav links, forms, and other content for toggling -->
