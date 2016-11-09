51a2b579a678e966eba00e098f8d1cc759cc309a
Namo <namo@namodev.com>
2016-11-09 23:33:06 +0700

    Added placeholder for iForgot

---
+++ resources/views/emails/iforgot.blade.php
@@ -0,0 +1,4 @@
+<b>iFORGOT MAIL DEMO (TODO: CHANGE!)</b><br />
+Applicant {{ $name }}, <br />
+
+To reset your password, please click <a href="https://apply.triamudom.ac.th/iforgot/link/{{ $token }}">here</a>.
