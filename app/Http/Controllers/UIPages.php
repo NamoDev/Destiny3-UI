d5f4dc2d7570449351abc66b3cea1a4ebc5c062e
theminer3746 <bwsminecraft@hotmail.com>
2016-11-10 14:48:11 +0700

    Untab

--- app/Http/Controllers/UIPages.php
+++ app/Http/Controllers/UIPages.php
@@ -17,44 +17,44 @@ use Session;
 
 class UIPages extends Controller {
 
-	// Public homepage
-	public function homePage() {
-		$visitingApplicant = new Applicant();
-		if ($visitingApplicant->isLoggedIn()) {
-			return redirect('application/home');
-		} else {
-			return response()->view('login_home');
-		}
-	}
+    // Public homepage
+    public function homePage() {
+        $visitingApplicant = new Applicant();
+        if ($visitingApplicant->isLoggedIn()) {
+            return redirect('application/home');
+        } else {
+            return response()->view('login_home');
+        }
+    }
 
-	// Registration homepage
-	public function newUserRegistrationPage() {
-		return response()->view('steps.00_new_user_registration');
-	}
+    // Registration homepage
+    public function newUserRegistrationPage() {
+        return response()->view('steps.00_new_user_registration');
+    }
 
-	// Applicant home page (a.k.a. the "Dashboard")
-	public function applicantDashboard() {
-		return response()->view('dashboard');
-	}
+    // Applicant home page (a.k.a. the "Dashboard")
+    public function applicantDashboard() {
+        return response()->view('dashboard');
+    }
 
-	// "Change Password" page for logged in users
-	public function changePasswordPage() {
-		return response()->view('change_password');
-	}
+    // "Change Password" page for logged in users
+    public function changePasswordPage() {
+        return response()->view('change_password');
+    }
 
-	// Application about page
-	public function aboutPage() {
-		return response()->view('about');
-	}
+    // Application about page
+    public function aboutPage() {
+        return response()->view('about');
+    }
 
-	// Frequently asked questions
-	public function faqPage() {
-		return response()->view('faq');
-	}
+    // Frequently asked questions
+    public function faqPage() {
+        return response()->view('faq');
+    }
 
-	public function unsupportedBrowser() {
-		return response()->view('unsupported_browser');
-	}
+    public function unsupportedBrowser() {
+        return response()->view('unsupported_browser');
+    }
 
     public function iForgotLandingPage(){
         return response()->view('iforgot_landing');
@@ -72,55 +72,55 @@ class UIPages extends Controller {
         return response()->view('iforgot_success');
     }
 
-	// Step 01: Applicant's basic information
-	public function step1_basicInfo() {
-		$applicantData = DB::collection("applicants")->where("citizen_id", Session::get("applicant_citizen_id"))->first();
+    // Step 01: Applicant's basic information
+    public function step1_basicInfo() {
+        $applicantData = DB::collection("applicants")->where("citizen_id", Session::get("applicant_citizen_id"))->first();
 
-		return response()->view('steps.01_basic_information', ['applicantData' => $applicantData]);
-	}
+        return response()->view('steps.01_basic_information', ['applicantData' => $applicantData]);
+    }
 
-	// Step 02: Applicant's parent information
-	public function step2_parentInfo() {
-		$applicantData = DB::collection("applicants")->where("citizen_id", Session::get("applicant_citizen_id"))->first();
+    // Step 02: Applicant's parent information
+    public function step2_parentInfo() {
+        $applicantData = DB::collection("applicants")->where("citizen_id", Session::get("applicant_citizen_id"))->first();
 
-		return response()->view('steps.02_parent_information', ['applicantData' => $applicantData]);
-	}
+        return response()->view('steps.02_parent_information', ['applicantData' => $applicantData]);
+    }
 
-	// Step 03: Applicant's address
-	public function step3_address() {
-		$applicantData = DB::collection("applicants")->where("citizen_id", Session::get("applicant_citizen_id"))->first();
+    // Step 03: Applicant's address
+    public function step3_address() {
+        $applicantData = DB::collection("applicants")->where("citizen_id", Session::get("applicant_citizen_id"))->first();
 
-		return response()->view('steps.03_address', ['applicantData' => $applicantData]);
-	}
+        return response()->view('steps.03_address', ['applicantData' => $applicantData]);
+    }
 
-	// Step 04: Applicant's educational history
-	public function step4_educationHistory() {
-		$applicantData = DB::collection("applicants")->where("citizen_id", Session::get("applicant_citizen_id"))->first();
+    // Step 04: Applicant's educational history
+    public function step4_educationHistory() {
+        $applicantData = DB::collection("applicants")->where("citizen_id", Session::get("applicant_citizen_id"))->first();
 
-		return response()->view('steps.04_education_history', ['applicantData' => $applicantData]);
-	}
+        return response()->view('steps.04_education_history', ['applicantData' => $applicantData]);
+    }
 
-	// Step 05: Plan selection
-	public function step5_planSelection() {
-		$applicantData = DB::collection("applicants")->where("citizen_id", Session::get("applicant_citizen_id"))->first();
+    // Step 05: Plan selection
+    public function step5_planSelection() {
+        $applicantData = DB::collection("applicants")->where("citizen_id", Session::get("applicant_citizen_id"))->first();
 
-		return response()->view('steps.05_plan_selection', ['applicantData' => $applicantData]);
-	}
+        return response()->view('steps.05_plan_selection', ['applicantData' => $applicantData]);
+    }
 
-	// Step 06: Application day selection
-	public function step6_applicationDaySelection() {
-		$applicantData = DB::collection("applicants")->where("citizen_id", Session::get("applicant_citizen_id"))->first();
-		$applicationDays = DB::collection("application_days")->get();
+    // Step 06: Application day selection
+    public function step6_applicationDaySelection() {
+        $applicantData = DB::collection("applicants")->where("citizen_id", Session::get("applicant_citizen_id"))->first();
+        $applicationDays = DB::collection("application_days")->get();
 
-		return response()->view('steps.06_application_day_selection', ['applicantData' => $applicantData, 'applicationDays' => $applicationDays]);
-	}
+        return response()->view('steps.06_application_day_selection', ['applicantData' => $applicantData, 'applicationDays' => $applicationDays]);
+    }
 
-	// Step 07: Document upload & verification
-	public function step7_uploadDocuments() {
-		$applicantData = DB::collection("applicants")->where("citizen_id", Session::get("applicant_citizen_id"))->first();
+    // Step 07: Document upload & verification
+    public function step7_uploadDocuments() {
+        $applicantData = DB::collection("applicants")->where("citizen_id", Session::get("applicant_citizen_id"))->first();
 
-		return response()->view('steps.07_document_upload', ['applicantData' => $applicantData]);
-	}
+        return response()->view('steps.07_document_upload', ['applicantData' => $applicantData]);
+    }
 
 
 }
