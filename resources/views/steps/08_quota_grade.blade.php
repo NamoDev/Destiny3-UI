e95ef361ba8a4963228508d2fb2e2794c3747ffb
Namo <namo@namodev.com>
2016-11-11 17:49:14 +0700

    Added icon for transcript page

--- resources/views/steps/08_quota_grade.blade.php
+++ resources/views/steps/08_quota_grade.blade.php
@@ -1,5 +1,6 @@
 @extends('layouts.master')
+@section('title', 'ประวัติผลการเรียน')
 
 @section('content')
-
+<legend><i class="fa fa-list"></i> ประวัติผลการเรียน <i class="fa fa-spinner fa-spin text-muted pull-right" style="display:none;" id="loadingSpinner"></i></legend>
 @endsection
