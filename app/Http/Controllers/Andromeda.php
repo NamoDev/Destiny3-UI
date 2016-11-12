d07d50f781443d2c85358d2a68eedc69a140a4e8
keenthekeen <keen@keendev.net>
2016-11-12 11:01:40 +0700

    Change email html content

--- app/Http/Controllers/Andromeda.php
+++ app/Http/Controllers/Andromeda.php
@@ -9,17 +9,17 @@ class Andromeda extends Controller {
         $token = (new \Lcobucci\JWT\Builder())->issuedBy(config('app.url'))// Configures the issuer (iss claim)
         ->canOnlyBeUsedBy(config('uiconfig.andromeda_url'))// Configures the audience (aud claim)
         ->issuedAt(time())// Configures the time that the token was issue (iat claim)
-        ->canOnlyBeUsedAfter(time() - 100000)// Configures the time that the token can be used (nbf claim)
-        ->expiresAt(time() + 100000); // Configures the expiration time of the token (nbf claim)
-        
+        ->canOnlyBeUsedAfter(time() - 50000)// Configures the time that the token can be used (nbf claim)
+        ->expiresAt(time() + 50000); // Configures the expiration time of the token (nbf claim)
+
         $visitingApplicant = new Applicant();
         if ($visitingApplicant->isLoggedIn()) {
             $applicant = Applicant::current();
             $token->set('userdata', ['name' => $applicant->title . $applicant->fname . ' ' . $applicant->lname, 'id' => $applicant->citizen_id, 'email' => $applicant->email]);
         }
-        
+
         $token = $token->sign((new \Lcobucci\JWT\Signer\Hmac\Sha256()), config('uiconfig.andromeda_key'))->getToken(); // Retrieves the generated token
         return redirect(config('uiconfig.andromeda_url') . '/helpme?attach=' . urlencode($token));
     }
-    
+
 }
