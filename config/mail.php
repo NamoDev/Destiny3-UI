742470e9da9c0f3adcf079ef821dd6ba0883edd9
keenthekeen <keen@keendev.net>
2016-11-12 11:17:39 +0700

    Implemented email sending capability

--- config/mail.php
+++ config/mail.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-
+    
     /*
     |--------------------------------------------------------------------------
     | Mail Driver
@@ -15,9 +15,9 @@ return [
     |            "ses", "sparkpost", "log"
     |
     */
-
+    
     'driver' => env('MAIL_DRIVER', 'smtp'),
-
+    
     /*
     |--------------------------------------------------------------------------
     | SMTP Host Address
@@ -28,9 +28,9 @@ return [
     | the Mailgun mail service which will provide reliable deliveries.
     |
     */
-
+    
     'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
-
+    
     /*
     |--------------------------------------------------------------------------
     | SMTP Host Port
@@ -41,9 +41,9 @@ return [
     | stay compatible with the Mailgun e-mail application by default.
     |
     */
-
+    
     'port' => env('MAIL_PORT', 587),
-
+    
     /*
     |--------------------------------------------------------------------------
     | Global "From" Address
@@ -54,9 +54,9 @@ return [
     | used globally for all e-mails that are sent by your application.
     |
     */
-
-    'from' => ['address' => null, 'name' => null],
-
+    
+    'from' => ['address' => NULL, 'name' => NULL],
+    
     /*
     |--------------------------------------------------------------------------
     | E-Mail Encryption Protocol
@@ -67,9 +67,9 @@ return [
     | transport layer security protocol should provide great security.
     |
     */
-
+    
     'encryption' => env('MAIL_ENCRYPTION', 'tls'),
-
+    
     /*
     |--------------------------------------------------------------------------
     | SMTP Server Username
@@ -80,9 +80,9 @@ return [
     | connection. You may also set the "password" value below this one.
     |
     */
-
+    
     'username' => env('MAIL_USERNAME'),
-
+    
     /*
     |--------------------------------------------------------------------------
     | SMTP Server Password
@@ -93,9 +93,9 @@ return [
     | connection so that the application will be able to send messages.
     |
     */
-
+    
     'password' => env('MAIL_PASSWORD'),
-
+    
     /*
     |--------------------------------------------------------------------------
     | Sendmail System Path
@@ -106,7 +106,12 @@ return [
     | been provided here, which will work well on most of your systems.
     |
     */
-
+    
     'sendmail' => '/usr/sbin/sendmail -bs',
+    
+    'mailgun' => array(
+        'domain' => env('MAILGUN_DOMAIN', 'apply.triamudom.ac.th'),
+        'secret' => env('MAILGUN_SECRET', 'your-mailgun-key'),
+    ),
 
 ];
