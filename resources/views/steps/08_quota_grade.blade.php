0e6af5ed66d9988b40a847437dadcb6244fffc1c
Namo <namo@namodev.com>
2016-11-18 01:41:11 +0700

    Added placebo "new subject" functionality for demonstration purposes. (Code will need to be rewritten for production)

--- resources/views/steps/08_quota_grade.blade.php
+++ resources/views/steps/08_quota_grade.blade.php
@@ -4,26 +4,34 @@
 @section('content')
 <legend><i class="fa fa-list"></i> ประวัติผลการเรียน <i class="fa fa-spinner fa-spin text-muted pull-right" style="display:none;" id="loadingSpinner"></i></legend>
 
-@for($i=0;$i<3;$i++)
-<div class="row">
-    <div class="col-xs-12 col-md-4">
-        <select class="form-control select select-primary select-block mbl">
-            <option value="s">ว (วิทยาศาสตร์)</option>
-            <option value="s">ค (คณิตศาสตร์)</option>
-            <option value="s">ส (สังคมศึกษา)</option>
-            <option value="s">ท (ภาษาไทย)</option>
-            <option value="s">อ (ภาษาอังกฤษ)</option>
-        </select>
-    </div>
-    <div class="col-xs-12 col-md-4">
-        <input type="text" class="form-control" placeholder="รหัสวิชา"></text>
-    </div>
-    <div class="col-xs-12 col-md-4">
-        <input type="text" class="form-control" placeholder="เกรด"></text>
+<div id="subjectsContainer">
+    <div class="row">
+        <div class="col-md-4">
+            <select class="form-control select select-primary select-block mbl">
+                <option value="sci">ว (วิทยาศาสตร์)</option>
+                <option value="mat">ค (คณิตศาสตร์)</option>
+                <option value="soc">ส (สังคมศึกษา)</option>
+                <option value="tha">ท (ภาษาไทย)</option>
+                <option value="eng">อ (ภาษาอังกฤษ)</option>
+            </select>
+        </div>
+        <div class="col-md-4">
+            <input type="text" class="form-control" placeholder="รหัสวิชา"></text>
+        </div>
+        <div class="col-md-3">
+            <input type="text" class="form-control" placeholder="คะแนนเฉลี่ยสะสม (เกรด)"></text>
+        </div>
+        <div class="col-xs-1">
+
+        </div>
     </div>
 </div>
-@endfor
 
+<div class="row">
+    <div class="col-md-12">
+        <a href="#" id="btnAddSubject" class="btn btn-success"><i class="fa fa-plus-circle"></i> เพิ่มรายวิชา</a>
+    </div>
+</div>
 
 <div class="row">
     <div class="col-xs-6 col-md-8">
@@ -33,4 +41,45 @@
         <button id="sendTheFormButton" class="btn btn-block btn-info">บันทึกข้อมูล</button>
     </div>
 </div>
+@endsection
+
+@section("additional_scripts")
+<script>
+
+var currentSubject = 1;
+
+$("#btnAddSubject").click(function(){
+    $("#subjectsContainer").append(" \
+    <div class=\"row dgrp\"> \
+        <div class=\"col-md-4\"> \
+            <select id=\"grpsel_" + currentSubject + "\" class=\"form-control select select-primary select-block mbl\"> \
+                <option value=\"sci\">ว (วิทยาศาสตร์)</option> \
+                <option value=\"mat\">ค (คณิตศาสตร์)</option> \
+                <option value=\"soc\">ส (สังคมศึกษา)</option> \
+                <option value=\"tha\">ท (ภาษาไทย)</option> \
+                <option value=\"eng\">อ (ภาษาอังกฤษ)</option> \
+            </select> \
+        </div> \
+        <div class=\"col-md-4\"> \
+            <input type=\"text\" class=\"form-control\" placeholder=\"รหัสวิชา\"></text> \
+        </div> \
+        <div class=\"col-md-3\"> \
+            <input type=\"text\" class=\"form-control\" placeholder=\"คะแนนเฉลี่ยสะสม (เกรด)\"></text> \
+        </div> \
+        <div class=\"col-xs-1\"> \
+            <a href='#' class='btnDeleteRow'><i class='fa fa-trash fa-2x'></i></a> \
+        </div> \
+    </div> \
+    ");
+
+    $("#grpsel_" + currentSubject).select2();
+    currentSubject++;
+
+});
+
+$('#subjectsContainer').on('click', '.btnDeleteRow', function(){
+    $(this).closest('.dgrp').remove();
+})
+
+</script>
 @endsection
