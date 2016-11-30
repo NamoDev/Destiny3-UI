511556adeaa7b4a16470cc6dcadf22cf354ef560
theminer3746 <bwsminecraft@hotmail.com>
2016-12-01 01:50:25 +0700

    Delete debug

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -935,8 +935,6 @@ class UserController extends Controller{
             // run the cURL query
             $result = curl_exec($ch);
             $returnHttpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-            Log::error($sendto);
-            Log::error($result);
             if(200 == $returnHttpCode){
                 return json_decode($result, true);
             }else{
