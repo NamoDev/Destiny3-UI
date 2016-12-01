7fb29684b3af76a684f623729f97df9656f14e93
theminer3746 <bwsminecraft@hotmail.com>
2016-12-01 07:34:06 +0700

    Edit unsupport browser

--- resources/views/unsupported_browser.blade.php
+++ resources/views/unsupported_browser.blade.php
@@ -1,2 +1,18 @@
 @extends('layouts.no_navbar')
 @section('title', 'เบราเซอร์ไม่รองรับ')
+
+@section('content')
+
+<div class="row" style="margin-top:120px;">
+    <div class="col-md-8 col-md-offset-2">
+        <div class="row">
+            <div class="col-sm-4">
+                <img src="/assets/images/woah.png" alt="O NOES" width="200px" />
+            </div>
+            <div class="col-sm-8">
+                <h3 style="margin-top:80px;">เบราเซอร์ไม่รองรับ</h3>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
