d270154ae45b3f05c642844fee7a8afc8d880034
theminer3746 <bwsminecraft@hotmail.com>
2016-12-01 14:43:54 +0700

    Add TokenMismatchException to not report

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -22,6 +22,7 @@ class Handler extends ExceptionHandler
         HttpException::class,
         ModelNotFoundException::class,
         ValidationException::class,
+        TokenMismatchException::class,
         Symfony\Component\HttpKernel\Exception\NotFoundHttpException::class,
     ];
 
