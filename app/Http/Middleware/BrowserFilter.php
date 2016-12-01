ea6de1427d37510a7740fc720cf615b49af900b7
theminer3746 <bwsminecraft@hotmail.com>
2016-12-01 07:33:52 +0700

    Fix typo

--- app/Http/Middleware/BrowserFilter.php
+++ app/Http/Middleware/BrowserFilter.php
@@ -8,52 +8,52 @@ use Jenssegers\Agent\Agent;
 use Illuminate\Http\Response;
 
 class BrowserFilter {
-	
-	/**
-	 * Agent instance
-	 *
-	 * @var Jenssegers\Agent\Agent
-	 */
-	private $agent;
-	
-	/**
-	 * Construct class
-	 *
-	 * @param
-	 * @return void
-	 */
-	public function __construct(Agent $agent) {
-		$this->agent = $agent;
-	}
-	
-	/**
-	 * Handle an incoming request.
-	 *
-	 * @param  \Illuminate\Http\Request $request
-	 * @param  \Closure                 $next
-	 * @return mixed
-	 */
-	public function handle($request, Closure $next) {
-		if ($this->agent->browser() == 'IE') {
-			return new Response(view('errors.unsupported_browser'));
-		} else {
-			$browser = $this->agent->browser();
-			$version = $this->agent->version($browser);
-			$version = explode('.', $version);
-			
-			if ($browser == 'Chrome') {
-				if ($version[0] < 38) {
-					return response()->view('unsupported_browser');
-				}
-			} else {
-				if ($browser == 'Firefox') {
-					if ($version[0] < 33) {
-						return response()->view('unsupported_browser');
-					}
-				}
-			}
-		}
-		
-		return $next($request);
-	}
+
+    /**
+     * Agent instance
+     *
+     * @var Jenssegers\Agent\Agent
+     */
+    private $agent;
+
+    /**
+     * Construct class
+     *
+     * @param
+     * @return void
+     */
+    public function __construct(Agent $agent) {
+        $this->agent = $agent;
+    }
+
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @param  \Closure                 $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next) {
+        if ($this->agent->browser() == 'IE') {
+            return new Response(view('unsupported_browser'));
+        } else {
+            $browser = $this->agent->browser();
+            $version = $this->agent->version($browser);
+            $version = explode('.', $version);
+
+            if ($browser == 'Chrome') {
+                if ($version[0] < 38) {
+                    return response()->view('unsupported_browser');
+                }
+            } else {
+                if ($browser == 'Firefox') {
+                    if ($version[0] < 33) {
+                        return response()->view('unsupported_browser');
+                    }
+                }
+            }
+        }
+
+        return $next($request);
+    }
 }
