d7464758adfbddd9e05b1c58534d393443f1e029
theminer3746 <bwsminecraft@hotmail.com>
2016-12-17 14:07:09 +0700

    Register schedule command

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -14,6 +14,7 @@ class Kernel extends ConsoleKernel
      */
     protected $commands = [
         // Commands\Inspire::class,
+        Commands\ReportApplicant::class,
     ];
 
     /**
@@ -24,7 +25,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')
-        //          ->hourly();
+        $schedule->command('reportapplicant sirirat.an@gmail.com')
+                 ->dailyAt('09:00');
     }
 }
