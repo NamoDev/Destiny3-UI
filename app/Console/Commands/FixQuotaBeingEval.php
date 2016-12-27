afad6e80e9251a4296721533370782b9b3267465
theminer3746 <bwsminecraft@hotmail.com>
2016-12-27 20:17:19 +0700

    Register new command to fix quota being evaluated stuck at 1

---
+++ app/Console/Commands/FixQuotaBeingEval.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class FixQuotaBeingEval extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'fixquotabeingeval';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Manual bug fix. Do not use until absolutly sure!!!';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        if($this->confirm('Are you sure you wish to continue? [y|N]')){
+            DB::collection('applicants')
+            ->where('quota_being_evaluated', 1)
+            ->where('evaluation_status', 0)
+            ->whereNotNull('comments')
+            ->update([
+                'quota_being_evaluated' => 0,
+            ]);
+
+            $this->info('Updated');
+        }else{
+            $this->info('Terminated');
+        }
+    }
+}
