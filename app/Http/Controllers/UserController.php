f54802d6eb1ec4e85e136108ca0eb60dd523baf7
theminer3746 <bwsminecraft@hotmail.com>
2017-01-06 16:57:40 +0700

    Revert change

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -510,7 +510,7 @@ class UserController extends Controller{
             }
             unset($i);
         }else{
-            Artisan::call('retrievedocument '.$data);
+            //Artisan::call('retrievedocument '.$data);
             if(!Storage::disk('document')->exists($data)){
                 Log::error('File not found', ['filepath' => $data]);
                 return RESTResponse::notFound('File not found (FileNotFoundException)');
